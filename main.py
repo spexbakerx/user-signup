33dc2caf8395c1eb6e670bf66c9a9400b5eb8ca5
Lauren Baker <spexbakerx@gmail.com>
2017-07-06 23:12:09 -0700

    fixed error messages

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from flask import Flask, request, redirect, render_template
-import cgi
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-@app.route('/signup')
+@app.route('/')
 def display_signup():
     return render_template('signup.html')
 
 
 def is_blank(text):
-    try:
-        text = ''
+    if text == '':
         return True
-    except ValueError:
-        return False
+
 
 
 @app.route('/signup', methods=['POST'])
@@ -32,25 +29,21 @@ def signup():
 
     if is_blank(username):
         username_error = 'Please enter a username'
-        username = ''
-    elif (' ' in username) == True:
+    elif ' ' in username:
         username_error = 'Username must not contain any spaces'
         username = ''
     else:
-        username = len(username)
-        if username > 3 or username < 20:
+        if len(username) < 3 or len(username) > 20:
             username_error = 'Username must be between 3 and 20 characters'
             username = ''
 
     if is_blank(password):
         password_error = 'Please enter a password'
-        password = ''
-    elif (' ' in password) == True:
+    elif ' ' in password:
         password_error = 'Password must not contain any spaces'
         password = ''
     else:
-        password = len(password)
-        if password > 3 or password < 20:
+        if len(password) < 3 or len(password) > 20:
             password_error = 'Password must be between 3 and 20 characters'
             password = ''
 
@@ -58,22 +51,24 @@ def signup():
         verify_error = 'Passwords do not match'
         verify = ''
 
-    if (' ' in email) == True:
+    if ' ' in email:
         email_error = 'Email must not contain any spaces'
         email = ''
+    elif email == '':
+        email =''
     elif email.count('@') != 1 or email.count('.') != 1:
         email_error = 'Please enter a valid email address'
         email = ''
     else:
-        email = len(email)
-        if email > 3 or email < 20:
+        if len(email) < 3 or len(email) > 20:
             email_error = 'Email must be between 3 and 20 characters'
             email = ''
 
     if not password_error and not username_error and not verify_error and not email_error:
-        return redirect('/welcome')
+        name = request.form['username']
+        return render_template('welcome.html', name=name)
     else:
-        return render_template('signup.html', username=username, password=password, verify=verify, email=email)
+        return render_template('signup.html', username=username, password=password, verify=verify, email=email, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
 
 @app.route('/welcome', methods=['POST'])
 def welcome():
